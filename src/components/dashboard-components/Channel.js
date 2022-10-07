f5310c558cda910d6c0cafd4522d72a87f2ee44d
Jhona Rica Manahan <manhan.jrd@gmail.com>
2022-10-07 19:54:50 +0800

    refactored channel component

--- src/components/dashboard-components/Channel.js
+++ src/components/dashboard-components/Channel.js
@@ -1,17 +1,20 @@
 
 export default function Channel(props) {
   const { channels, onChannelClick } = props
-  return(
-    <>
-    <div className="h-30 overflow-y-auto flex flex-col" id="custom-style">
-      {channels?.map((channel) => (
-          <button className="hover:bg-fuchsia-900 text-left w-fit py-1 px-1" 
+
+  const channelList = channels?.map(channel => {
+    return <button className="hover:bg-fuchsia-900 text-left w-fit py-1 px-1" 
             key={channel.name} 
             onClick={onChannelClick}>
             # 
-          <span className="text-sm">{channel.name}</span>
-          </button>
-      ))}
+            <span className="text-sm">{channel.name}</span>
+           </button>
+  })
+
+  return(
+    <>
+    <div className="h-30 overflow-y-auto flex flex-col" id="custom-style">
+      {channelList}
     </div>
   </>
   )
