52fadc407ca8682b4c42159d0582f2dc815a338c
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-10-08 10:52:43 +0800

    hotfix in chatmessage, slight ui fix in chatbox

--- src/components/dashboard-components/Chatmessages.js
+++ src/components/dashboard-components/Chatmessages.js
@@ -1,6 +1,6 @@
 const Chatmessages = (props) => {
   const { messageList } = props
-  const filtered = messageList.filter((value, index, self) =>
+  const filtered = messageList?.filter((value, index, self) =>
   index === self.findIndex((t) => (
     t.id === value.id && t.body === value.body
   ))
@@ -8,7 +8,7 @@ const Chatmessages = (props) => {
   return (
     <>
     {<div className="overflow-y-auto	h-85">
-      {filtered.map((data) => (
+      {filtered?.map((data) => (
           <div className="ml-2 mb-5" 
           key={data.id}>
             <div className="text-xs mt-4">{data.sender.email}</div>
--- src/components/dashboard-components/DmList.js
+++ src/components/dashboard-components/DmList.js
@@ -4,9 +4,10 @@ const DmList = (props) => {
   const { userList, setSelectedPage, setReceipient, setReceipientID} = props;
  
   const renderUserList = (list) => {
-    return userList.length > 0 ? list.map(data => (<button className="pl-4 mt-4" 
-                                    onClick ={()=> {setReceipient(data.user); setReceipientID(data.id); setSelectedPage("direct")}} 
-                                    ><span className="text-sm hover:bg-fuchsia-900 py-2 px-1" key={data.id}>{data.user}</span></button>)) : <div>No Messages</div>
+    return userList.length > 0 ? 
+    list.map(data => (<button className="pl-4 mt-4" 
+    onClick ={()=> {setReceipient(data.user); setReceipientID(data.id); setSelectedPage("direct")}}>
+    <span className="text-sm hover:bg-fuchsia-900 py-2 px-1" key={data.id}>{data.user}</span></button>)) : <div>No Messages</div>
   }
 
   return (
